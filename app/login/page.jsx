c81b4932214483d7868a52620842e86f2390d821
Kavindu Amarasinghe <amarasinghakavindu09@gmail.com>
2024-12-01 11:57:32 +0530

    login page Make

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+export default function LoginPage(){
+  return(
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-300">
+      <div className="text-gray-600 text-center bg-green-300 px-5 py-3 rounded-lg" >Login</div>
+      </div>
+  )
+}
+
+
